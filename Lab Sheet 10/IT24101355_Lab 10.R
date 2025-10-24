2e770cd3b6a912a527f4a1909391f6a12a624ec1
IT24101355 <IT24101355@my.sliit.lk>
2025-10-24 11:00:41 +0530

    IT24101355_PS_Lab 10

---
+++ Lab Sheet 10/IT24101355_Lab 10.R
@@ -0,0 +1,24 @@
+setwd("C:\\Users\\User\\OneDrive - Sri Lanka Institute of Information Technology\\Desktop\\IT24101355_Lab 10")
+getwd()
+
+# Chi-Square Goodness-of-Fit Test for Snack Types
+
+#Question 01
+
+snack_types <- c("A", "B", "C", "D")
+observed <- c(120, 95, 85, 100)
+
+#Question 02
+expected_prob <- c(0.25, 0.25, 0.25, 0.25)
+
+#Question 03
+test_result <- chisq.test(x = observed, p = expected_prob)
+
+
+print("Chi-Square Goodness-of-Fit Test Results:")
+print(test_result)
+
+
+print("Expected counts:")
+print(test_result$expected)
+
